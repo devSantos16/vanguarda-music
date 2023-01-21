d1e7542db4cae80aa452235bc98264d73c743ea3
Emanuel dos Santos Paz <devsantos1999@gmail.com>
2023-01-21 16:40:31 -0300

    Insert all music names in a custom Playlist

--- src/commands/got.js
+++ src/commands/got.js
@@ -10,14 +10,23 @@ module.exports = {
 			'https://www.youtube.com/watch?v=kMl7HJ4DvgI&ab_channel=RaminDjawadi-Topic',
 			'https://www.youtube.com/watch?v=F0o1x9c4XcA&ab_channel=RaminDjawadi-Topic'
 		]
-
+		
 		const playlist = await client.DisTube.createCustomPlaylist(songs, {
 			member: interaction.member,
 			properties: { name: "Game of Thrones Playlist", source: "custom" },
 			parallel: true
 		})
+		
 
-		await interaction.reply(`Playlist de Game of Thrones`).catch(e => { })
+		let messagePlaylistName = "";
+		let i = 1;
+		playlist.songs.forEach(element => {
+			messagePlaylistName += `${i}rd: ${element.name}\n`;
+			i++;
+		});
+		
 		client.DisTube.play(interaction.member.voice.channel, playlist)
+
+		await interaction.reply(`✅ Now playing ${playlist.name} | All Playlist Here:\n\n${messagePlaylistName}`).catch(e => { })
 	},
 };
